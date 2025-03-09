b232bf0b70813d70fdfa986db3024ba98504fe94
Marharyta Reznychenko <margaret.rez1401@gmail.com>
2025-03-09 20:45:16 +0200

    Delete src directory

--- src/services/contacts.js
+++
@@ -1,9 +0,0 @@
-import { ContactsCollection } from '../db/models/contacts.js';
-
-export const getAllContacts = async () => {
-  return await ContactsCollection.find();
-};
-
-export const getContactById = async (contactId) => {
-  return await ContactsCollection.findById(contactId);
-};
